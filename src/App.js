f2fe1ce8280ee43fab63ed3be902918ffe77472f
Sanchit Gupta <sanchitgupta456@gmail.com>
2021-03-31 20:20:05 +0530

    Added Loading and Icon

--- src/App.js
+++ src/App.js
@@ -6,19 +6,50 @@ import About from "./Components/About";
 import Experience from "./Components/Experience";
 import Footer from "./Components/Footer";
 import ContactUs from "./Components/ContactUs";
+import { useEffect, useState } from "react";
+import React from "react";
+import RingLoader from "react-spinners/RingLoader";
+import { Container } from "@material-ui/core";
+
 // import RenderCard from "./Components/Card";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#FF0009");
+
+  useEffect(() => {
+    setLoading(true);
+    document.title = "Sanchit Gupta";
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <div>
-      <Nav />
-      <Banner />
-      <About />
-      <Services />
-      <Experience />
-      <ContactUs />
-      <Footer />
-    </div>
+    <React.Fragment>
+      {loading ? (
+        <div className="loader">
+          <RingLoader
+            color={color}
+            loading={loading}
+            size={300}
+            height={300}
+            width={100}
+          />
+        </div>
+      ) : (
+        // <div>Hello</div>
+        <div>
+          <Nav />
+          <Banner />
+          <About />
+          <Services />
+          <Experience />
+          <ContactUs />
+          <Footer />
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
